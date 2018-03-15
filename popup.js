9ca8bc1c0db782f171e062aad97652b572d2381d
Fabien <fabien.orea@etu.u-bordeaux.fr>
2018-03-15 16:32:36 +0100

    add table on the popup.html

--- popup.js
+++ popup.js
@@ -5,10 +5,43 @@ function generateList(data) {
       return b.numRequests - a.numRequests;
     });
 
-    var section = document.querySelector('body>section');
+    var table = document.querySelector('table>tbody');
+    for (var i = 0; i < data.length; i++) {
+      var tr = document.createElement('tr');
+      var rank = document.createElement('td');
+      rank.textContent = i;
+      
+      var url = document.createElement('td');
+      url.textContent = data[i]['url'];
+      
+      var title = document.createElement('td');
+      title.textContent = data[i]['title'];
+
+      var keywords = document.createElement('td');
+      if (data[i]['keywords'] != null) {
+        data[i]['keywords'].forEach( function(element) {
+          keywords.textContent += element+","; 
+        });   
+      }
+      else {
+        keywords.textContent += "nothing"; 
+      }
+
+      var views = document.createElement('td');
+      views.textContent = data[i]['numRequests'];
+
+      tr.appendChild(rank);
+      tr.appendChild(url);
+      tr.appendChild(title);
+      tr.appendChild(keywords);
+      tr.appendChild(views);
+      table.appendChild(tr);
+    }
+
+
+    var liste = document.getElementById('dropdown');
     var ol = document.createElement('ol');
     var li, p, em, code, title, keywords, numReq;
-    var i;
     for (var i = 0; i < data.length; i++) {
       li = document.createElement('li');
       p = document.createElement('p');
@@ -44,8 +77,8 @@ function generateList(data) {
       li.appendChild(p);
       ol.appendChild(li);
     }
-    section.innerHTML = '';
-    section.appendChild(ol);
+    liste.innerHTML = '';
+    liste.appendChild(ol);
   }
   else {
     console.log('fail data')

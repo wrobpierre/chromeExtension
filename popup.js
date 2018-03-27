9985350970c07149bd1e9fa4a3e23b6b6a587ce6
WROBLEWSKI <wrobpierre@hotmail.fr>
2018-03-27 15:23:38 +0100

    merge

--- popup.js
+++ popup.js
@@ -1,3 +1,59 @@
+var example = {
+  data: [
+  {'url':'https://www.lci.fr/', 'title':'lci', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 0}, 'views':10, 'scrollPercent': 0},
+  {'url':'https://fr.wikipedia.org/', 'title':'wikipedia', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 10, 'secondes': 0}, 'views':30, 'scrollPercent': 0},
+  {'url':'https://www.youtube.com/', 'title':'youtube', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 2, 'secondes': 0}, 'views':3, 'scrollPercent': 0},
+  {'url':'http://www.le-dictionnaire.com/', 'title':'dictionnaire', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 6, 'secondes': 0}, 'views':1000, 'scrollPercent': 0},
+  {'url':'https://stackoverflow.com/', 'title':'stackoverflow', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 15}, 'views':1235, 'scrollPercent': 0},
+  {'url':'http://www.meteofrance.com/', 'title':'meteo france', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 15, 'secondes': 1}, 'views':46, 'scrollPercent': 0},
+  {'url':'http://www.jeuxvideo.com/', 'title':'jeuxvideo.com', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 3, 'secondes': 30}, 'views':5464, 'scrollPercent': 0},
+  {'url':'http://www.lemonde.fr/', 'title':'lemonde', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 10, 'secondes': 1}, 'views':158, 'scrollPercent': 0},
+  {'url':'https://www.google.fr/maps', 'title':'google maps', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 4, 'secondes': 0}, 'views':10546, 'scrollPercent': 0},
+  {'url':'https://translate.google.fr/?hl=fr', 'title':'translate', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 14, 'secondes': 0}, 'views':7896, 'scrollPercent': 0},
+  {'url':'https://www.youtube.com/user/joueurdugrenier', 'title':'JDG', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 30}, 'views':12, 'scrollPercent': 0},
+  {'url':'http://www.larousse.fr/', 'title':'larousse', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 40}, 'views':751, 'scrollPercent': 0},
+  {'url':'https://openclassrooms.com/', 'title':'openclassrooms', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 2, 'secondes': 0}, 'views':2135, 'scrollPercent': 0},
+  {'url':'https://conjugaison.lemonde.fr/', 'title':'lemonde conjugaison', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 12, 'secondes': 0}, 'views':7, 'scrollPercent': 0},
+  {'url':'http://www.lcp.fr/', 'title':'lcp', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 16, 'secondes': 0}, 'views':654, 'scrollPercent': 0},
+  {'url':'https://www.tf1.fr/', 'title':'TF1', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 2, 'secondes': 0}, 'views':1865, 'scrollPercent': 0},
+  {'url':'https://doc.ubuntu-fr.org', 'title':'doc ubuntu', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 25}, 'views':9874, 'scrollPercent': 0},
+  {'url':'http://www.quebecscience.qc.ca/', 'title':'quebecscience', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 6, 'secondes': 0}, 'views':46, 'scrollPercent': 0},
+  {'url':'http://philosophie.philisto.fr/', 'title':'philosophie.philisto', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 9, 'secondes': 10}, 'views':1235, 'scrollPercent': 0},
+  {'url':'https://fr.wikipedia.org/wiki/Temps', 'title':'wikipedia', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 7, 'secondes': 0}, 'views':964, 'scrollPercent': 0},
+  {'url':'https://www.mondedemain.org/', 'title':'mondedemain', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 10, 'secondes': 6}, 'views':264, 'scrollPercent': 0},
+  {'url':'http://www.linternaute.com/', 'title':'linternaute', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 11, 'secondes': 0}, 'views':12358, 'scrollPercent': 0},
+  {'url':'http://forums.futura-sciences.com/', 'title':'futura-sciences', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 5, 'secondes': 0}, 'views':1235, 'scrollPercent': 0},
+  {'url':'https://www.journaldunet.com/', 'title':'journaldunet', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 14, 'secondes': 1}, 'views':6874, 'scrollPercent': 0},
+  {'url':'https://www.pourlascience.fr/', 'title':'pourlascience', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 15, 'secondes': 23}, 'views':3, 'scrollPercent': 0},
+  {'url':'http://www.hatem.com/temps.htm', 'title':'hatem', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 13, 'secondes': 0}, 'views':658, 'scrollPercent': 0},
+  {'url':'https://www.franceinter.fr/', 'title':'franceinter', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 2, 'secondes': 0}, 'views':102, 'scrollPercent': 0},
+  {'url':'https://www.kaizen-magazine.com/', 'title':'kaizen-magazine', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 6, 'secondes': 0}, 'views':7892, 'scrollPercent': 0},
+  {'url':'http://etienneklein.fr/quest-ce-que-le-temps/', 'title':'etienneklein', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 10, 'secondes': 0}, 'views':2365, 'scrollPercent': 0},
+  {'url':'https://www.lelivrescolaire.fr/', 'title':'lelivrescolaire', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 23}, 'views':86, 'scrollPercent': 0},
+  {'url':'https://usbeketrica.com', 'title':'usbeketrica', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 4, 'secondes': 0}, 'views':4862, 'scrollPercent': 0},
+  {'url':'http://www.vie-publique.fr/', 'title':'vie-publique', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 12, 'secondes': 0}, 'views':123, 'scrollPercent': 0},
+  {'url':'http://dicocitations.lemonde.fr/citation-temps.php', 'title':'dicocitations.lemonde', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 2, 'secondes': 0}, 'views':1234, 'scrollPercent': 0},
+  {'url':'http://www.assistant-juridique.fr/', 'title':'assistant-juridique', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 15}, 'views':10236, 'scrollPercent': 0},
+  {'url':'https://la-philosophie.com/', 'title':'philosophie', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 17}, 'views':913, 'scrollPercent': 0},
+  {'url':'https://www.assistancescolaire.com/', 'title':'assistancescolaire', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 12, 'secondes': 0}, 'views':78, 'scrollPercent': 0},
+  {'url':'https://www.cafephilosophia.fr/', 'title':'cafephilosophia', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 8, 'secondes': 0}, 'views':5236, 'scrollPercent': 0},
+  {'url':'http://olivierabel.fr', 'title':'olivierabel', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 1, 'secondes': 0}, 'views':785, 'scrollPercent': 0},
+  {'url':'http://www.maphilo.net/', 'title':'maphilo', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 7, 'secondes': 12}, 'views':569, 'scrollPercent': 0},
+  {'url':'http://www.philocours.com/', 'title':'philocours', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 3, 'secondes': 50}, 'views':1235, 'scrollPercent': 0},
+  {'url':'https://dicophilo.fr/', 'title':'dicophilo', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 4, 'secondes': 0}, 'views':10356, 'scrollPercent': 0},
+  {'url':'http://www.philopratique.com/', 'title':'philopratique', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 15, 'secondes': 0}, 'views':986, 'scrollPercent': 0},
+  {'url':'https://www.franceculture.fr/', 'title':'franceculture', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 14, 'secondes': 0}, 'views':5648, 'scrollPercent': 0},
+  {'url':'http://www.philolog.fr/', 'title':'philolog', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 13, 'secondes': 0}, 'views':1298, 'scrollPercent': 0},
+  {'url':'http://www.philosophiecontresuperstition.com/', 'title':'philosophiecontresuperstition', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 3, 'secondes': 0}, 'views':654, 'scrollPercent': 0},
+  {'url':'https://www.scienceshumaines.com/', 'title':'scienceshumaines', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 0, 'secondes': 10}, 'views':130, 'scrollPercent': 0},
+  {'url':'http://www.letudiant.fr/', 'title':'letudiant', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 17, 'secondes': 0}, 'views':9, 'scrollPercent': 0},
+  {'url':'https://www.bac-l.net/', 'title':'bac-l', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 25, 'secondes': 0}, 'views':491, 'scrollPercent': 0},
+  {'url':'http://www.lesclefsdelecole.com/', 'title':'lesclefsdelecole', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 6, 'secondes': 0}, 'views':1236, 'scrollPercent': 0},
+  {'url':'http://www.studyrama.com/', 'title':'studyrama', 'keywords':['LCI', 'information', 'en direct'], 'dateBegin': "", 'timeOnPage': {'hours': 0, 'minutes': 12, 'secondes': 12}, 'views':9865, 'scrollPercent': 0}
+  
+  ]
+}
+
 function generateList(data) {
   if (data != undefined) {
 
@@ -19,7 +75,7 @@ function generateList(data) {
 
       var keywords = document.createElement('td');
       if (data[i]['keywords'] != null) {
-          keywords.textContent = data[i]['keywords'].join(', '); 
+        keywords.textContent = data[i]['keywords'].join(', '); 
       }
       else {
         keywords.textContent = "nothing..."; 
@@ -113,13 +169,18 @@ function sendData(key) {
   var storage = chrome.storage.local;
 
   storage.get('data', function(result){
-    var post = $.post('http://localhost/chromeExtension/dataBase.php', { d:result, key:key });
 
+    if (key == "load") {
+      var post = $.post('http://localhost/entrainement japon/extension2/chromeExtension/dataBase.php', { d:example, key:"add" });
+    }
+    else{
+      var post = $.post('http://localhost/entrainement japon/extension2/chromeExtension/dataBase.php', { d:result, key:key });
+    }
     post.done(function(data) {
-      var test = $.parseJSON(data);
-      $.each($.parseJSON(test[0]['timer']), function(i, element){
-        console.log(element)
-      });
+      // var test = $.parseJSON(data);
+      // $.each($.parseJSON(test[0]['timer']), function(i, element){
+        console.log(data);
+      // });
     });
   });
 }

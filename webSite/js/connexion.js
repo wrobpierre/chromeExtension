9bf67681e1bb6fea4b88289d33185646cc3e3ef3
WROBLEWSKI <wrobpierre@hotmail.fr>
2018-05-24 16:00:02 +0100

    Register safe (check email, check already exist, check twice pwd and email)

--- webSite/js/connexion.js
+++ webSite/js/connexion.js
@@ -1,26 +1,35 @@
 $(document).ready(function(){
- 
+
     $("#submit").click(function(e){
         e.preventDefault();
- 
-        $.post(
-            '../connexion/connexion.php',
-            {
-                email : $("#email").val(),
-                password : md5($("#password").val())
-            },
- 
-            function(data){
-                console.log(data);
-                if(data == 'Success'){
-                     document.location.href="../index.php";
-                }
-                else{ 
-                     $("#resultat").html("<p>Erreur lors de la connexion...</p>");
-                }
-         
-            },
-            'text'
-         );
+        if(validateEmail($("#email").val())){
+            $.post(
+                '../connexion/connexion.php',
+                {
+                    email : $("#email").val(),
+                    password : md5($("#password").val())
+                },
+
+                function(data){
+                    console.log(data);
+                    if(data == 'Success'){
+                       document.location.href="../index.php";
+                   }
+                   else{ 
+                       $("#resultat").html("<p>Your email or password is invalid.</p>");
+                   }
+
+               },
+               'text'
+               );
+        }
+        else{
+            $("#resultat").html("<p>Email is invalid.</p>");
+        }
     });
 });
+function validateEmail(email) 
+{
+  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(email);
+}
--- webSite/js/register.js
+++ webSite/js/register.js
@@ -1,28 +1,53 @@
 $(document).ready(function(){
- 
-    $("#submit").click(function(e){
+
+     $("#submit").click(function(e){
         e.preventDefault();
- 
-        $.post(
-            '../connexion/register.php',
-            {
-                email : $("#email").val(),
-                password : md5($("#password").val()),
-                job : $("#job").val(),
-                name : $("#name").val()
-            },
- 
-            function(data){
-                if(data == 'Success'){
-                     alert("Your account is create !")
-                     document.location.href="../index.php";
-                }
-                else{
-                     $("#resultat").html("<p>Erreur lors de la connexion...</p>");
-                }
-         
-            },
-            'text'
-         );
+        if($("#email").val() == $("#emailCheck").val() && $("#password").val() == $("#passwordCheck").val() && validateEmail($("#email").val())){
+            $.post(
+                '../connexion/register.php',
+                {
+                    email : $("#email").val(),
+                    password : md5($("#password").val()),
+                    job : $("#job").val(),
+                    name : $("#name").val()
+                },
+
+                function(data){
+                    console.log(data);
+                    if(data == 'Success'){
+                       alert("Your account is create !")
+                       document.location.href="../index.php";
+                   }
+                   else if(data == 'Failed'){
+                       $("#resultat").html("<p>Bad email or password.</p>");
+                   }
+                   else if(data == 'Exist'){
+                    $("#resultat").html("<p>Your mail address is already used.</p>");
+                   }
+
+               },
+               'text'
+               );
+        }
+        else{
+            if($("#email").val() != $("#emailCheck").val()){
+                $("#resultat").html("<p>The two email are not the same.</p>");
+            }
+            else if($("#password").val() != $("#passwordCheck").val()){
+                $("#resultat").html("<p>The two password are not the same.</p>");
+            }
+            else if(!validateEmail($("#email").val())){
+                $("#resultat").html("<p>Your email is not valid.</p>");
+            }
+            else{
+                $("#resultat").html("<p>A problem accured please try later.</p>");
+            }
+        }
     });
 });
+
+function validateEmail(email) 
+{
+  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(email);
+}

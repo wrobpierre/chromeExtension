b275ad9a7deb10418488e516fa1cfd34af8a639b
WROBLEWSKI <wrobpierre@hotmail.fr>
2018-05-28 14:58:56 +0100

    Adding redirection when user is disconnect

--- webSite/questionnaires/add_questionnaire.php
+++ webSite/questionnaires/add_questionnaire.php
@@ -1,3 +1,9 @@
+<?php
+if (!isset($_SESSION['user'])) {
+	header("Location: ../index.php");
+	exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
--- webSite/questionnaires/edit_questionnaire.php
+++ webSite/questionnaires/edit_questionnaire.php
@@ -1,3 +1,9 @@
+<?php
+if (!isset($_SESSION['user'])) {
+    header("Location: destination.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>

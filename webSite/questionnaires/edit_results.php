97cd02aa63e713833ad33366f4bc5788162cf753
WROBLEWSKI <wrobpierre@hotmail.fr>
2018-06-18 14:29:13 +0100

    Add CSS on edit_result

--- webSite/questionnaires/edit_results.php
+++ webSite/questionnaires/edit_results.php
@@ -25,15 +25,26 @@ else{
 	<meta charset="utf-8">
 	<title>Edit users' results</title>
 </head>
+<?php include '../layout/includes.php'; ?>
 <body>
-	<form method="post" action="management_questionnaire.php">
-		<input type="hidden" name="action" value="edit_user_result">
-		<ul class="users">
-			
-		</ul>
-		
-		<input type="submit" value="Send" class="button_valid">
-	</form>
+	<?php include '../layout/header.php'; ?>
+	<header class="parallax-window" data-parallax="scroll" style="height: 300px;" data-image-src="../img/list_user.jpg"></header>	
+	<div id="form-questionnaire" class="w3-container w3-light-grey">
+		<div class="w3-col l3 w3-padding"></div>
+		<div class="w3-col l6 w3-col s12 w3-border w3-white w3-margin-bottom" style="margin-top:-80px;">
+			<form method="post" action="management_questionnaire.php">
+				<h1 class="w3-red w3-padding" style="margin:0;">Rate user responses</h1>
+				<input type="hidden" name="action" value="edit_user_result">
+				<ul class="users" style="list-style: none; padding: 0;">
+
+				</ul>
+				<div class="w3-col l12 w3-center w3-section">
+					<input type="submit" value="Send" class="w3-button w3-green w3-large">
+					
+				</div>
+			</form>
+		</div>
+	</div>
 
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script type="text/javascript">
@@ -41,6 +52,7 @@ else{
 		//var adress = "http://localhost/chromeExtension"
 
 		var param = document.URL.split('-')[1];
+		var nbUsers = 0;
 		//console.log(param); 
 		var post = $.post(adress+'/webSite/questionnaires/management_questionnaire.php', { action:"get_user_result", id:param });
 		post.done(function(data){
@@ -48,14 +60,21 @@ else{
 				dataParse = JSON.parse(data);
 				console.log(dataParse);
 				$.each(dataParse, function(index,value){
-					var user = $('<li>User '+index+'</li>');
+					if(nbUsers%2 == 0){
+						var user = $('<li class="w3-section w3-white w3-padding"><h2>User '+index+'</h2></li>');
+					}
+					else{
+						var user = $('<li class="w3-section" style="background: #f5f6fa;"><h2 class="w3-padding">User '+index+'</h2></li>');
+
+					}
+					nbUsers++;
 					var ul = $('<ul></ul>');
 					$.each(value, function(i,v){
 						var li = $('<li></li>');
 						var question = $('<p>'+v.question+'</p>');
 						var answer = $('<p>'+v.answer+'</p>');
-						var right = $('<input type="radio" name="res['+index+']['+v.key_question+']" value="right"> <label>Right</label>');
-						var wrong = $('<input type="radio" name="res['+index+']['+v.key_question+']" value="wrong"> <label>Wrong</label>');
+						var right = $('<input type="radio" class="w3-radio w3-margin" name="res['+index+']['+v.key_question+']" value="right"> <label>Right</label>');
+						var wrong = $('<input type="radio" class="w3-radio w3-margin" name="res['+index+']['+v.key_question+']" value="wrong"> <label>Wrong</label>');
 						li.append(question,answer,right,wrong);
 						ul.append(li);
 					})
@@ -71,5 +90,7 @@ else{
 			};
 		});
 	</script>
+	<script src="../js/parallax.js-1.5.0/parallax.js" type="text/javascript"></script>
+	<?php include '../layout/footer.php'; ?>
 </body>
 </html>
